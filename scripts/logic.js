a6942abc8a4aa845e4bac3c9cda84c72bd117349
Pavel <dvdtygc@gmail.com>
2018-02-26 01:52:38 +0200

    Main logic update

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,9 +1,13 @@
 let timeStarted = false;
-let time = 5;
+let time = 59;
 let timer_dom = document.getElementById("timer");
 let words_dom = document.getElementById("words");
 let typing_box_dom = document.getElementById("typing_box");
+let current_word = '';
+let current_word_dom = '';
 let wpm_dom = document.getElementById("wpm");
+let right_words = 0;
+let wrong_words = 0;
 
 function getRandomWord() {
     return words[Math.floor(Math.random() * words.length)];
@@ -15,7 +19,7 @@ function initApp() {
 }
 
 function setWords(count = 10) {
-    let setter = '<span class="word current-word">' + getRandomWord() + '</span> ';
+    let setter = '<span class="word current_word">' + getRandomWord() + '</span> ';
 
     for (let i = 0; i < count - 1; i++) {
         setter += '<span class="word">' + getRandomWord() + '</span> ';
@@ -24,19 +28,37 @@ function setWords(count = 10) {
     words_dom.innerHTML = setter;
 }
 
-function startTimer() {
+function tick() {
     if (!timeStarted) {
         timer();
         timeStarted = true;
     }
 }
 
+document.body.onkeydown = e => {
+    if (e.keyCode === 32) {
+        current_word_dom = document.getElementsByClassName("current_word")[0];
+        current_word = current_word_dom.innerText;
+
+        if (typing_box_dom.value.trim() === current_word) {
+            right_words++;
+        } else {
+            wrong_words++;
+        }
+
+        typing_box_dom.value = '';
+        current_word_dom.remove();
+        document.getElementsByClassName("word")[0].className += " current_word";
+        words_dom.innerHTML += '<span class="word">' + getRandomWord() + '</span> ';
+    }
+};
+
 function timer() {
-    setInterval(_ => {
+    let x = setInterval(_ => {
         if (time > 1) {
             timer_dom.innerHTML = --time;
         } else {
-            clearInterval(timer);
+            clearInterval(x);
             gameOver();
         }
     }, 1000);
@@ -45,6 +67,8 @@ function timer() {
 function gameOver() {
     timer_dom.innerHTML = 0;
     typing_box_dom.disabled = "disabled";
+    console.log("right words: " + right_words);
+    console.log("wrong words: " + wrong_words);
 }
 
 initApp();
